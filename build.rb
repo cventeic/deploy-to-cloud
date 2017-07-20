2c8f7d1aa2304c7ce674e9a975dab0f15036f48f
Chris Venteicher <chris@venteicher.org>
2017-07-20 11:27:44 -0500

    pass RAILS_MASTER_KEY and use correct names and tags

--- build.rb
+++ build.rb
@@ -6,8 +6,8 @@ require 'ostruct'
 
 project = OpenStruct.new
 project.id   = 0
-project.name = 'test-app'
-project.tag  = 'test-app'
+project.name = 'venteicher-org'
+project.tag  = 'venteicher-org'
 # project.gcs_bucket_name = ''
 # project.access_key = ''
 # project.secret = ''
@@ -28,32 +28,23 @@ project.tag  = 'test-app'
 
 #   c. Containerizing the application
 #image_tag =  "gcr.io/#{project.id}/#{project.name}:#{project.tag}"
-#image_url =  "gcr.io/#{project.id}/#{project.name}:latest"
-
 #registry  =  "gcr.io/#{project.id}/#{project.name}:#{project.tag}"
+#
+#image_uri =  "gcr.io/#{project.id}/#{project.name}:latest"
+#image_uri = "#{external_registry_endpoint}#{project.name}"
+#image_uri = "gcr.io/#{project.id}/#{project.name}:#{project.image_uri}"
 
 # docker run -d -p 5000:5000 --name registry registry:2
 
+#exec 'rails new venteicher-org-git --force'
 
-#image_tag =  "#{registry}:#{project.tag}"
-#image_tag =  "#{registry}:latest"
-#image_url =  "gcr.io/#{project.id}/#{project.name}:latest"
-
-#
-#exec 'rails new test-app-git --force'
-
-#external_registry_endpoint  = "localhost:5000/"
-#image_url = "#{external_registry_endpoint}#{project.name}"
-
-image_url = "test-app"
+image_uri = "venteicher-org"
+image_uri = "gcr.io/venteicher-org-174023/venteicher-org"
 docker_context_url = "docker_context"
 
-#container_name = "gcr.io/#{project.id}/#{project.name}:#{project.container_name}"
-container_name = "test-app"
+docker_create_container_image(image_uri, docker_context_url)
 
-docker_create_container_image(container_name, docker_context_url)
-
-#gce_push_container_image(image_tag)
+gce_push_container_image(image_uri)
 
 # Start local docker registry
 # docker run -d -p 5000:5000 --name registry registry:2
@@ -63,34 +54,45 @@ docker_create_container_image(container_name, docker_context_url)
 
 kubernetes_delete_old()
 
+
+kubernetes_config_url = "kubernetes_config"
+
+puts "input rails_master_key:"
+rails_master_key = gets.chomp
+
+puts "your key is #{rails_master_key}"
+
+
+kubernetes_establish_app_secrets_yml(kubernetes_config_url, rails_master_key)
+
+
 # Deploy the replicated back end for the application.
 #
 #component = OpenStruct.new
-#component.app = "test-app"
+#component.app = "venteicher-org"
 #component.tier  = "worker"
 #component.replicas = 2
-#kubernetes_establish_deployment(component, image_url)
+#kubernetes_establish_deployment(component, image_uri)
 
 
 # Deploy the replicated front end for the application.
 #
 component = OpenStruct.new
-component.app = "test-app"
+component.app = "venteicher-org"
 component.tier  = "frontend"
 component.replicas = 3
 
-kubernetes_config_url = "kubernetes_config"
-kubernetes_establish_deployment(component, image_url, kubernetes_config_url)
+kubernetes_establish_deployment(component, image_uri, kubernetes_config_url)
 
 # Deploy the load-balanced service to route HTTP traffic to the front end.
 #
 service = OpenStruct.new
-service.app = "test-app"
+service.app = "venteicher-org"
 service.tier  = "frontend"
 kubernetes_establish_service(service, component, kubernetes_config_url)
 
 # To see app on minikube do this
-# minikube service test-app-load-balancer
+# minikube service venteicher-org-load-balancer
 
 
 
